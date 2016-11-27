dd1ad2f4a60d6659d95fbd9d54e578ca801b42cf
Kjili <Kjili@users.noreply.github.com>
2016-11-28 00:21:12 +0100

    chore(assets): fix convert script
    fix convert script to work from outside the assets folder
    
    add requirement

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# convert requires imagemagick package
+
+cd assets
 convert player.xcf player.png
 convert player.png -colorspace Gray enemy.png
 convert enemy.png -fill "#920000" -opaque "#787878" enemy.png
