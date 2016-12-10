fc3cbd07394794ff94e737eac8fbd5128728a4a4
Kjili <Kjili@users.noreply.github.com>
2016-12-10 23:02:45 +0100

    chore(assets): add conversions for intro
    add necessary conversions of images to png for playing the intro
    animation

--- assets/convert.sh
+++ assets/convert.sh
@@ -3,3 +3,9 @@
 convert player.xcf player.png
 convert player.png -colorspace Gray enemy.png
 convert enemy.png -fill "#920000" -opaque "#787878" enemy.png
+
+# create intro png's
+
+cp player.png ../game/scenes/intro/player.png
+cp enemy.png ../game/scenes/intro/enemy.png
+convert intro.xcf ../game/scenes/intro/intro.png
