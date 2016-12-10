ee082afe40788b21845a11360132640254d8b06e
Kjili <Kjili@users.noreply.github.com>
2016-12-10 20:42:00 +0100

    Revert "chore(assets): fix convert script"
    This reverts commit dd1ad2f4a60d6659d95fbd9d54e578ca801b42cf.

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# convert requires imagemagick package
-
-cd assets
 convert player.xcf player.png
 convert player.png -colorspace Gray enemy.png
 convert enemy.png -fill "#920000" -opaque "#787878" enemy.png
