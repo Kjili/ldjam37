326106bd314c179dcc5df8accf79d8868fce0577
Kjili <Kjili@users.noreply.github.com>
2016-12-11 19:01:47 +0100

    feat(scenes): add enemy
    add enemy, fix hero and enemy initial positions

--- assets/convert.sh
+++ assets/convert.sh
@@ -13,8 +13,10 @@ convert intro.xcf ../game/scenes/intro/intro.png
 # create battleground
 convert -layers flatten battleground.xcf ../game/scenes/battleground/battleground.png
 
-# create hero animations
+# create hero and enemy animations
 for f in animation/*
 do
 	convert $f ../game/scenes/hero/$(basename ${f%.xcf}).png
+	convert $f -colorspace Gray ../game/scenes/enemy/$(basename ${f%.xcf}).png
+	convert ../game/scenes/enemy/$(basename ${f%.xcf}).png -fill "#920000" -opaque "#787878" ../game/scenes/enemy/$(basename ${f%.xcf}).png
 done
